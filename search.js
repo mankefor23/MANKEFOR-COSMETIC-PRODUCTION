022857ef3dd141ea5be247e179ad4b8aff8d7f31
mankefor23 <pmankefor@gmail.com>
2025-09-04 06:29:00 -0700

    initial commit

---
+++ search.js
@@ -0,0 +1,7 @@
+let prpduct=[
+    {
+        product: ' precious liquid',
+        keyword:[ 'liquid', 'essence', 'wash', ' bleach', 'about', 'product', 'contact', 'buy now'],
+        imges: ['/images/500.pngc']
+    }
+]

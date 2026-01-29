f8e63eebb89bcf449f6d5b1b0b4af9ef3442d360
Cheneba <chenebametuge14@gmail.com>
2026-01-29 02:52:27 +0100

    Did some organization in order to ensure smooth addition of a backend system to this frontend

--- login.js
+++
@@ -1,9 +0,0 @@
-const wrapper = document.querySelector(".wrapper");
-const signupBtn = document.querySelector(".signup-btn");
-const loginBtn = document.querySelector(".login-btn");
-signupBtn.addEventListener("click", () => {
-  wrapper.classList.add("active");
-});
-loginBtn.addEventListener("click", () => {
-  wrapper.classList.remove("active");
-});
